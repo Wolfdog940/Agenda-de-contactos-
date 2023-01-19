2b00237f640a50b28ee5402c0fe610702908be4f
wolfdog940 <oscarurrutia940@gmail.com>
2023-01-19 13:43:43 +0100

    update

--- routes/contacts.py
+++ routes/contacts.py
@@ -7,9 +7,10 @@ contacts = Blueprint("contacts", __name__)
 
 @contacts.route("/")
 def home():
+
     contacts=Contact.query.all()
     return render_template("index.html",contacts=contacts)
-    
+
 
 
 @contacts.route("/new" ,methods =["POST"] )
@@ -27,17 +28,33 @@ def add_contact():
     return redirect(url_for("contacts.home"))
 
 
-@contacts.route("/update/<int:id>")
+@contacts.route("/update/<int:id>" , methods=["POST","GET"])
 def update(id):
-    return "update a contact"
+
+    contact= Contact.query.get(id)
+
+    if request.method=="POST":
+        
+        contact= Contact.query.get(id)
+        contact.fullname= request.form["fullname"]
+        contact.email=request.form["email"]
+        contact.phone=request.form["phone"]
+        db.session.commit()
+        return redirect(url_for("contacts.home"))
+
+    
+    
+    return render_template("update.html",contact=contact)
 
 
 
 @contacts.route("/delete/<int:id>")
 def delete(id):
+
     contact= Contact.query.get(id)
     db.session.delete(contact)
     db.session.commit()
+
     return redirect(url_for("contacts.home"))
 
 
